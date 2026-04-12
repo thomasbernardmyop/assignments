92927616bdfbd53639fcf78ab988aaccb58902c7
hidemyassbaby <49113438+hidemyassbaby@users.noreply.github.com>
2026-04-13 09:28:47 +1200

    Revert "Update"
    This reverts commit ff7d8d755ba57c12d7c7004d6ea5cce633df7a71.

--- BIT502 Fundamentals of Programming/BIT502 Assessments/Assessment 3/Working on/database.py
+++
@@ -1,8 +0,0 @@
-#-------------------------------#
-#     BIT502 - Assessment 3     #
-#         Thomas Bernard        #
-#      Student ID 5142644       #
-#-------------------------------#
-
-# database.py
-# This file contains the database functions for the application. 
--- BIT502 Fundamentals of Programming/BIT502 Assessments/Assessment 3/Working on/membership_form.py
+++
@@ -1,8 +0,0 @@
-#-------------------------------#
-#     BIT502 - Assessment 3     #
-#         Thomas Bernard        #
-#      Student ID 5142644       #
-#-------------------------------#
-
-# membership_form.py
-# This file contains the membership form for the application.
